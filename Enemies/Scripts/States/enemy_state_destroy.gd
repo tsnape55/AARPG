9749944cd62fcbd6c26d30ce0bd0a405e578f9c6
tsnape55 <tomsnape55@gmail.com>
2026-08-18 14:38:40 +0100

    enemy drops

--- Enemies/Scripts/States/enemy_state_destroy.gd
+++ Enemies/Scripts/States/enemy_state_destroy.gd
@@ -1,13 +1,19 @@
 class_name EnemyStateDestroy extends EnemyState
 
+const PICKUP = preload("res://Items/item_pickup/item_pickup.tscn")
+
 @export var anim_name : String = "destroy"
 @export var knockback_speed: float = 200.0
 @export var decelerate_speed: float = 10.0
 @onready var hurt_box: HurtBox = $"../../HurtBox"
 
+@export_category("Item Drops")
+@export var drops: Array[DropData]
+
+
+
 var _damage_position: Vector2
 var _direction : Vector2
-var _animation_finished: bool = false
 
 func init() -> void:
 	enemy.enemy_destroyed.connect(_on_enemy_destroyed)
@@ -22,6 +28,8 @@ func enter() -> void:
 	
 	enemy.update_animation(anim_name)
 	enemy.animation_player.animation_finished.connect(_on_animation_finished)
+	disable_hurt_box()
+	drop_items()
 	pass
 	
 func exit() -> void:
@@ -40,4 +48,26 @@ func _on_enemy_destroyed(_hurt_box: HurtBox) -> void:
 	
 func _on_animation_finished(_a: String) -> void:
 	enemy.queue_free()
+	
+func disable_hurt_box() -> void:
+	var current_hurt_box: HurtBox = enemy.get_node_or_null("HurtBox")
+	if current_hurt_box:
+		current_hurt_box.monitoring = false
+		
+func drop_items() -> void:
+	if drops.size() == 0:
+		return
+		
+	for i in drops.size():
+		if drops[i] == null || drops[i].item == null:
+			continue
+		else:
+			var drop_count: int = drops[i].get_drop_count()
+			for j in drop_count:
+				var drop: ItemPickup = PICKUP.instantiate() as ItemPickup
+				drop.item_data = drops[i].item
+				enemy.get_parent().call_deferred("add_child", drop)
+				drop.global_position = enemy.global_position
+				drop.velocity = enemy.velocity.rotated(randf_range(-1.5, 1.5)) * randf_range(0.9, 1.5)
+		
 	
--- Items/item_pickup/item_pickup.gd
+++ Items/item_pickup/item_pickup.gd
@@ -1,5 +1,5 @@
 @tool 
-class_name ItemPickup extends Node
+class_name ItemPickup extends CharacterBody2D
 
 @export var item_data: ItemData : set = set_item_data
 
@@ -15,6 +15,13 @@ func _ready() -> void:
 	area_2d.body_entered.connect(on_body_entered)	
 	
 	pass
+	
+func _physics_process(delta: float) -> void:
+	var collision_info = move_and_collide(velocity * delta)
+	if collision_info:
+		velocity = velocity.bounce(collision_info.get_normal())
+	velocity -= velocity * delta * 4
+		
 
 func set_item_data(value: ItemData) -> void:
 	update_texture()
